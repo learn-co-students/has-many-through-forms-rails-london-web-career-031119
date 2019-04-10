1ad55209fdc717942baf326b555ada62ed3cf0f1
Cully-Curwen <github email address>
2019-04-10 18:28:48 +0100

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+fiction = Category.create(name: 'fiction')
+non_fiction = Category.create(name: 'non-fiction')
